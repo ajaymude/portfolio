7c4c52242c84107d52fc3d5e5a6e765690fdd42b
ajaymude <mudeajay@gmail.com>
2024-05-31 17:19:34 +0530

    portfolio 1st  commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:mudeajay@email.com" target="_blank" >mudeajayl@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/ajaymude" target="_blank">linkedin.com/in/ajaymude</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/ajaymude" target="_blank">github.com/ajaymude</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+import resume from '../../../assets/resume/ajay mude resume.pdf'
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ajay Mude</h1>
+        <p className={styles.description}>
+          Empowering digital landscapes through holistic expertise: UI design,
+          frontend, backend, and app development for transformative user
+          experiences.
+        </p>
+        <div className={styles.link}>
+          <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+            Contact
+          </a>
+
+          <a href={resume} download className={styles.contactBtn}>
+            Resume
+          </a>
+        </div>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
